079549274a952e223a79661b04ebf73823938fd9
AlexArciszewski <arciszew@yahoo.com>
2025-07-15 17:14:53 +0200

    Initial commit

---
+++ 0001_Series_pandas.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+#Series
+
+some_series = pd.Series([1, 2, 3, 4, 5])
+print(some_series)
+
+# 0    1
+# 1    2
+# 2    3
+# 3    4
+# 4    5
+# dtype: int64
